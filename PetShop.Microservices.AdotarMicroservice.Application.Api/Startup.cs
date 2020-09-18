e852fc6cd9a61335a29542abc099a2698c1befef
Sergio Bianchi <ss.bianchi@gmail.com>
2020-09-18 00:12:56 -0300

    Swagger Adotar API

--- PetShop.Microservices.AdotarMicroservice.Application.Api/Startup.cs
+++ PetShop.Microservices.AdotarMicroservice.Application.Api/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using PetShop.Microservices.AdotarMicroservice.Infra.DataAccess.Contexts;
 
 namespace PetShop.Microservices.AdotarMicroservice.Application.Api
@@ -38,11 +39,37 @@ namespace PetShop.Microservices.AdotarMicroservice.Application.Api
                     options.RequireHttpsMetadata = false;
                     options.ApiName = "AdotarMicroservice_ApiResource";
                 });
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Adotar Microservice API",
+                    Description = "Current api for PetShop created by Sergio Bianchi",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Sergio Bianchi",
+                        Email = string.Empty,
+                        Url = new Uri("https://github.com/ssbianchi"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
+            //Enable middleware to serve swagger - ui(HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Adotar Microservice API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
